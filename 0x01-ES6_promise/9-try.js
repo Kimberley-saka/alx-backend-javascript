786de442e610f447f23cb8459138daef5bc22d9e
Kimberley saka <kimberleysaka@gmail.com>
2023-07-02 13:29:54 +0200

    solution to task 9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+export default function guardrail(mathFunction) {
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(error.toString());
+  }
+  queue.push('Guardrail was processed');
+  return queue;
+}
