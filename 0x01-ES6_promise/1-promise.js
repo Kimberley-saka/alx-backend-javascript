3cc70128e8a047719fd9384366fc46fb6305bdfc
Kimberley saka <kimberleysaka@gmail.com>
2023-07-02 09:57:41 +0200

    solution to task 1

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+/* eslint-disable linebreak-style */
+
+function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
+
+export default getFullResponseFromAPI;
