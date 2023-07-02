69c720a242200423434c59ef978bef9e0014a7a6
Kimberley saka <kimberleysaka@gmail.com>
2023-07-02 10:09:49 +0200

    solution to task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+/* eslint-disable linebreak-style */
+function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });
+}
+
+export default handleResponseFromAPI;
