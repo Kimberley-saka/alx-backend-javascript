ce042340f6bb8f6c22a597b38f53f708d1131ab4
Kimberley saka <kimberleysaka@gmail.com>
2023-07-03 15:48:19 +0200

    solution to task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,35 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-param-reassign */
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    newCurrency = new Currency();
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency._name} (${this.currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return (amount * conversionRate);
+  }
+}
