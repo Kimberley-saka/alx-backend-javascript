d23282668e9e98a287df5b118696825b6e0bd6a8
Kimberley saka <kimberleysaka@gmail.com>
2023-07-04 16:41:29 +0200

    solution to task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,11 @@
+/* eslint-disable linebreak-style */
+function getListStudents() {
+  const students = [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+  return students;
+}
+
+console.log(getListStudents());
