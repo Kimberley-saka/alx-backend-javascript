049b2706c0f73512e000f3372bb2427807165bfe
Kimberley saka <kimberleysaka@gmail.com>
2023-07-04 17:46:31 +0200

    solution to task 6

---
+++ 0x03-ES6_data_manipulation/6-main.js
@@ -0,0 +1,4 @@
+/* eslint-disable linebreak-style */
+import setFromArray from './6-set';
+
+console.log(setFromArray([12, 32, 15, 78, 98, 15]));
---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,5 @@
+/* eslint-disable linebreak-style */
+export default function setFromArray(arrayOfAnyKind) {
+  const set = new Set(arrayOfAnyKind);
+  return set;
+}
