f3d310d9f363304306c8e94f67323ced2387775c
Kimberley saka <kimberleysaka@gmail.com>
2023-07-17 09:39:28 +0200

    solution to task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable class-methods-use-this */
+class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
