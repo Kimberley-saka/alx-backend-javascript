b484b969235138775b280d25b20d82c88c0f19dc
Kimberley saka <kimberleysaka@gmail.com>
2023-09-19 17:25:14 +0200

    simple express app

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
+
+module.exports = app;
