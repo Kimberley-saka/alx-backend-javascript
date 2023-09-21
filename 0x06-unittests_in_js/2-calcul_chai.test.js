df45b27cb8312ac836a1ccc84ffc707f0e6dc22e
Kimberley saka <kimberleysaka@gmail.com>
2023-09-21 12:50:35 +0200

    updated

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,30 +1,82 @@
-const {expect} = require('chai');
+const {expect} = require( 'chai');
 const calculateNumber = require('./1-calcul');
 
-describe('calculateNumber', function () {
-  describe('SUM operation', function () {
-    it('add rounded numbers', function () {
+escribe('calculateNumber', function () {
+  describe('SUM no Round', function () {
+    it('should return 5', function () {
+      expect(calculateNumber('SUM', 1, 4)).to.equal(5);
+    });
+  });
+
+  describe('SUM first round', function () {
+    it('should return 6', function () {
+      expect(calculateNumber('SUM', 2.4, 4)).to.equal(6);
+    });
+  });
+
+  describe('SUM second round ', function () {
+    it('should return 6', function () {
+      expect(calculateNumber('SUM', 4, 2.4)).to.equal(6);
+    });
+  });
+
+  describe('SUM both round', function () {
+    it('should return 6', function () {
       expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
-      expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
     });
   });
 
-  describe('SUBTRACT operation', function () {
-    it('subtract rounded b from a', function () {
+  describe('SUBTRACT no round', function () {
+    it('should return 2', function () {
+      expect(calculateNumber('SUBTRACT', 5, 3)).to.equal(2);
+    });
+  });
+
+  describe('SUBTRACT first round', function () {
+    it('should return -3', function () {
+      expect(calculateNumber('SUBTRACT', 2, 4.5)).to.equal(-3);
+    });
+  });
+
+  describe('SUBTRACT second round', function () {
+    it('should return 3', function () {
+      expect(calculateNumber('SUBTRACT', 4.5, 2)).to.equal(3);
+    });
+  });
+
+  describe('SUBTRACT both round', function () {
+    it('should return -4', function () {
       expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
-      expect(calculateNumber('SUBTRACT', 5, 2.5)).to.equal(2);
     });
   });
 
-  describe('DIVIDE operation', function () {
-    it('divide a by rounded b', function () {
-      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
-      expect(calculateNumber('DIVIDE', 1, 3.7)).to.equal(0.25);
+  describe('DIVIDE no round', function () {
+    it('should return 2', function () {
+      expect(calculateNumber('DIVIDE', 8, 4)).to.equal(2);
     });
+  });
 
-    it('handle division by zero', function () {
+  describe('DIVIDE first round', function () {
+    it('should return 5', function () {
+      expect(calculateNumber('DIVIDE', 9.5, 2)).to.equal(5);
+    });
+  });
+
+  describe('DIVIDE second round', function () {
+    it('should return 0.2', function () {
+      expect(calculateNumber('DIVIDE', 2, 9.5)).to.equal(0.2);
+    });
+  });
+
+  describe('DIVIDE both round', function () {
+    it('should return 0.2', function () {
+      expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+    });
+  });
+
+  describe('DIVIDE Error', function () {
+    it('should return Error', function () {
       expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
-      expect(calculateNumber('DIVIDE', 0, 0)).to.equal('Error');
     });
   });
 });
