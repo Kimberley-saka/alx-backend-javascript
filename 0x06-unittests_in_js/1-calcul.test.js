e0f1acc8782b69fee25daf2524c859123fe7fdef
Kimberley saka <kimberleysaka@gmail.com>
2023-09-21 11:40:20 +0200

    solutions to task

---
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -0,0 +1,30 @@
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', function () {
+  describe('SUM operation', function () {
+    it('add rounded numbers', function () {
+      assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
+      assert.strictEqual(calculateNumber('SUM', 1.2, 3.7), 5);
+    });
+  });
+
+  describe('SUBTRACT operation', function () {
+    it('subtract rounded b from a', function () {
+      assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+      assert.strictEqual(calculateNumber('SUBTRACT', 5, 2.5), 2);
+    });
+  });
+
+  describe('DIVIDE operation', function () {
+    it('divide a by rounded b', function () {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+      assert.strictEqual(calculateNumber('DIVIDE', 10, 3.3), 3);
+    });
+
+    it('handle division by zero', function () {
+      assert.strictEqual(calculateNumber('DIVIDE', 1.4, 0), 'Error');
+      assert.strictEqual(calculateNumber('DIVIDE', 0, 0), 'Error');
+    });
+  });
+});
